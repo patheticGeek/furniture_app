ba40c8edf543795836d85385b62b6af9a84ebfed
Pathetic Geek <geekpathetic@gmail.com>
2020-08-30 17:42:22 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:furniture_app/constants.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +12,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Furniture app',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
+        primaryColor: kPrimaryColor,
+        accentColor: kPrimaryColor,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(),
